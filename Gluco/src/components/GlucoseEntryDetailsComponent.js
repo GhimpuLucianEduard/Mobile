c5e1d94b59b7565c7df6dd65fa9944a3ba2674ca
Lucian Ghimpu <ghimpulucianeduard@gmail.com>
2019-01-09 01:28:30 +0200

    wtv

--- Gluco/src/components/GlucoseEntryDetailsComponent.js
+++ Gluco/src/components/GlucoseEntryDetailsComponent.js
@@ -1,19 +1,159 @@
 import React, { Component } from 'react'
-import { FlatList, StyleSheet, Text, View, ActivityIndicator, TouchableHighlight, AsyncStorage } from 'react-native';
-import { Card, ListItem, Button } from 'react-native-elements'
+import { Alert, TimePickerAndroid, TouchableOpacity, FlatList, StyleSheet, Text, View, ActivityIndicator, TouchableHighlight, AsyncStorage } from 'react-native';
+import { CheckBox, FormLabel, FormInput, FormValidationMessage, Card, ListItem, Button } from 'react-native-elements';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import { withNavigation } from 'react-navigation';
+import DatePicker from 'react-native-datepicker'
+import DateTimePicker from 'react-native-modal-datetime-picker';
 
 export default class GlucoseEntryDetailsComponent extends Component {
+
+  state = {
+    entry: {},
+    isSave: false,
+    isFirstLoad: true,
+    afterMeal: true,
+    value: "",
+    date: "2019-01-10",
+    time: "00:00"
+  }
+
+  _showDateTimePicker = async () => {
+    try {
+      const { action, hour, minute } = await TimePickerAndroid.open({
+        hour: 14,
+        minute: 0,
+        is24Hour: true, // Will display '2 PM'
+      });
+      var minToSave = "";
+      if (minute === 0) {
+        minToSave = "00";
+      } else {
+        minToSave = minute;
+      }
+      const timedroid = hour + ":" + minToSave;
+      this.setState({
+        time: timedroid
+      })
+      if (action !== TimePickerAndroid.dismissedAction) {
+      }
+    } catch ({ code, message }) {
+      console.warn('Cannot open time picker', message);
+    }
+  }
+
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      entry: props.navigation.state.params.entry,
+      isSave: props.navigation.state.params.isSave,
+      isFirstLoad: true,
+      afterMeal: props.navigation.state.params.entry.afterMeal,
+      value: `${props.navigation.state.params.entry.value}`,
+      date: "2019-01-10",
+      time: "00:00"
+    }
+
+   //console.log(this.state.entry);
+  }
+
   render() {
 
     const { navigation } = this.props;
-    const itemId = navigation.getParam('text', 'NO-ID');
 
     return (
       <View>
-        <Text>itemId: {JSON.stringify(itemId)}</Text>
+        <FormLabel>Value</FormLabel>
+        <FormInput onChangeText={(text) => this._inputChanged(text)} value={this.state.value} placeholder="type value here..."></FormInput>
+        <FormLabel>After Meal</FormLabel>
+        <CheckBox
+          checked={this.state.afterMeal}
+          onPress={() => this._onChangeCheck()}
+          title='Click Here'
+        />
+        <FormLabel>Date</FormLabel>
+        <DatePicker
+          style={{ width: 200 }}
+          date={this.state.date}
+          mode="date"
+          placeholder="select date"
+          format="YYYY-MM-DD"
+          minDate="1971-05-01"
+          confirmBtnText="Confirm"
+          cancelBtnText="Cancel"
+          customStyles={{
+            dateIcon: {
+              position: 'absolute',
+              left: 0,
+              top: 4,
+              marginLeft: 0
+            },
+            dateInput: {
+              marginLeft: 36
+            }
+          }}
+          onDateChange={(date) => {
+            this.setState({ date: date });
+            console.log(this.state.date);
+            this._showDateTimePicker();
+          }}
+        />
+        <FormLabel>Time</FormLabel>
+        <FormInput value={this.state.time} editable={false} placeholder="time"></FormInput>
+
+
+        <Button
+          onPress={() => this._onSaveClicked()}
+          raised
+          title='SAVE' />
+
       </View>
     )
+  }
+
+  _inputChanged = (text) => {
+    console.log(`Input changedL: ${text}`)
+    this.setState({
+      value: text
+    })
+  }
+
+  _onSaveClicked = () => {
+    var msg = "";
+    const valueNr = this.state.value;
+    if (valueNr === "" || parseInt(valueNr) < 0 || parseInt(valueNr) > 400) {
+      msg += "Value invalid! \n";
+    }
+    if (msg !== "") {
+      Alert.alert(
+        'Error',
+        msg,
+        [
+          { text: 'OK', onPress: () => console.log('OK Pressed') },
+        ],
+        { cancelable: false }
+      )
+    } else {
+      var dateAr = this.state.date.split("-");
+      var hourAr = this.state.time.split(":");
+
+
+      var dateTemp = new Date(this.state.date);
+      dateTemp.setHours(hourAr[0]);
+      dateTemp.setMinutes(hourAr[1]);
+      var timestampJs = dateTemp.getTime();
+      var entryFS = {...this.state.entry};
+      entryFS.value = valueNr;
+      entryFS.timestamp = timestampJs;
+      //pop  request and save async
+      this.props.navigation.state.params.returnData(entryFS);
+      this.props.navigation.goBack();
+    }
+  }
+
+  _onChangeCheck = () => {
+    this.setState({ afterMeal: !this.state.afterMeal });
+    console.log(this.state.afterMeal);
   }
 }
--- Gluco/src/components/ListViewComponent.js
+++ Gluco/src/components/ListViewComponent.js
@@ -3,38 +3,35 @@ import { FlatList, StyleSheet, Text, View, ActivityIndicator, TouchableHighlight
 import { Card, ListItem, Button } from 'react-native-elements'
 import Icon from 'react-native-vector-icons/FontAwesome';
 import { withNavigation } from 'react-navigation';
-import EditButtonComponent from './EditButtonComponent';
+import axios from 'axios'
 // import { axios } from "axios"
 
 class ListViewComponent extends Component {
 
     state = {
         data: [],
-        isLoading: true
+        isLoading: true,
+        listFlag: 0
     }
 
     componentDidMount() {
-        return fetch(
-            'http://10.0.2.2:329/glucose', {
-                method: 'GET',
-                headers: {
-                    Authorization: '"Baerer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6Imx1Y2ZzZGZzZGlhbjIzQGEuY29tIiwidXNlcklkIjoiNWMyN2NlMmZmZGRjMGM0Mzc0ZDdhNGVlIiwiaWF0IjoxNTQ2MTE0NDI5LCJleHAiOjE2MzI1MTQ0Mjl9.g-kzQtnm3fnflXMsAbVFnrx4RajpV8wviareTsKPQw4',
-                }
-            })
-            .then((response) => response.json())
-            .then((responseJson) => {
 
-                console.log(responseJson);
-
+        instance.get('/glucose')
+            .then((response) => {
                 this.setState({
-                    data: responseJson,
+                    data: response.data,
                     isLoading: false
                 });
             })
-            .catch((error) => {
-                console.error(error);
+            .catch(function (error) {
+                // handle error
+                console.log(error);
+            })
+            .then(function () {
+                console.log("final");
             });
     }
+
 
     render() {
 
@@ -49,6 +46,7 @@ class ListViewComponent extends Component {
         return (
             <View style={styles.container}>
                 <FlatList
+                    extraData={this.state.listFlag}
                     keyExtractor={(item) => item._id}
                     data={this.state.data}
                     renderItem={({ item }) => (
@@ -58,12 +56,13 @@ class ListViewComponent extends Component {
                                 <TouchableHighlight onPress={() => this._onPress(item)}>
                                     <Icon style={{ alignSelf: "flex-end" }} name="remove" size={32} />
                                 </TouchableHighlight>
-                                <TouchableHighlight onPress={() => { this.props.navigation.navigate('Details', { text: item}) }}>
+                                <TouchableHighlight onPress={() => { this.props.navigation.navigate('Details', { entry: item, isSave: false, returnData: this.returnData.bind(this) }) }}>
                                     <Icon style={{ alignSelf: "flex-end" }} name="edit" size={32} />
                                 </TouchableHighlight>
                             </View>
                             <View style={{ flex: 1, flexDirection: "row" }}>
                                 <Text style={{ flex: 1 }}>{item.date}</Text>
+                                <Text>{getDate(item.timestamp)}</Text>
                                 <Text style={{ alignSelf: "flex-end" }}>{getAfterMeal(item.afterMeal)}</Text>
                             </View>
                         </Card>
@@ -84,12 +83,50 @@ class ListViewComponent extends Component {
             console.log("NU MERGE")
         }
     }
+
+
+    returnData = (entryJson) => {
+        console.log(entryJson);
+        var entries = this.state.data;
+        var updated = false;
+        for (var i in entries) {
+            if (entries[i]._id == entryJson._id) {
+                entries[i] = entryJson;
+                updated = true;
+                break; //Stop this loop, we found it!
+            }
+        }
+        if (!updated) {
+            entries.push(entryJson)
+        }
+
+        this.setState({
+            data: entries,
+            listFlag: 1
+        });
+    }
 }
 
 export default withNavigation(ListViewComponent);
 
+const instance = axios.create({
+    baseURL: 'http://10.0.2.2:329',
+    timeout: 1000,
+    headers: { 'Authorization': 'Baerer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6Imx1Y2ZzZGZzZGlhbjIzQGEuY29tIiwidXNlcklkIjoiNWMyN2NlMmZmZGRjMGM0Mzc0ZDdhNGVlIiwiaWF0IjoxNTQ2MTE0NDI5LCJleHAiOjE2MzI1MTQ0Mjl9.g-kzQtnm3fnflXMsAbVFnrx4RajpV8wviareTsKPQw4' }
+});
+
 function getAfterMeal(isAfter) {
     return isAfter ? "After Meal" : "Before Meal";
+}
+
+function getDate(timestamp) {
+
+    var date = new Date(timestamp);
+    return date.getFullYear() + "-"
+    + (date.getMonth()+1)  + "-" 
+    + date.getDate() + "  "  
+    + date.getHours() + ":"  
+    + date.getMinutes();
 }
 
 const styles = StyleSheet.create({
